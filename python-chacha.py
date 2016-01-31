374352facc91eb1f6a0230d460e206fa9f076b12
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2016-01-31 13:23:21 -0600

    intermediate commit

--- python-chacha.py
+++ python-chacha.py
@@ -10,57 +10,96 @@ import subprocess
 import serial
 from vendor.pyWattsup import WattsUp
 
-TRIALS = 10
+TRIALS = 5
 
 ################################################################################
 
 if len(sys.argv) != 4:
-    print('Usage: {} <coretype> <fstype> <writeto>'.format(sys.argv[0]))
+    print('Usage: {} <coretype> <writeto>'.format(sys.argv[0]))
     sys.exit(1)
 
 coreType = sys.argv[1]
-fsType = sys.argv[2]
-writeto = sys.argv[3]
+writeto = sys.argv[2]
 
 trials = TRIALS
 
 wattsup = WattsUp('/dev/ttyUSB0', 115200, verbose=False)
 
-with open('/home/odroid/bd3/rsync/energy-AES-1/results/shmoo.{}.{}.results'.format(coreType, fsType), 'a') as out:
+def trial(description, fn):
+    print(description)
+    print('waiting for write buffer flush...')
+    
+    time.sleep(2)
+
+    try:
+        wattsup.serial.open()
+    except serial.serialutil.SerialException:
+        pass
+
+    wattsup.clearMemory()
+    wattsup.logInternal(1)
+    
+    # Begin logging with Wattsup (above), run filebench (here), close out the
+    # Wattsup logger (below)
+    print("command returned: ", fn)
+
+    # This loop handles any annoying errors we may encounter
+    while True:
+        try:
+            wattsup.printStats(wattsup.getInternalData(), out)
+            wattsup.serial.close()
+            break
+
+        except ValueError:
+            print('[+] recovered from ValueError')
+            wattsup.serial.close()
+            time.sleep(0.1) # Give Wattsup a moment to get its shit together
+            wattsup.serial.open()
+            continue
+
+with open('/home/odroid/bd3/rsync/energy-AES-1/results/shmoo.{}.results'.format(coreType), 'a') as out:
     while trials:
         trials = trials - 1
         trial = TRIALS-trials
-        print('beginning trial {} of {}'.format(trial, TRIALS))
 
+        trial('beginning trial {}-1-1 of {} (small.random, AES-CTR)'.format(trial, TRIALS), '')
+
+        print('beginning trial {}-1-2 of {} (small.random, ChaCha20)'.format(trial, TRIALS))
         print('waiting for write buffer flush...')
+        
         time.sleep(2)
 
-        try:
-            wattsup.serial.open()
-        except serial.serialutil.SerialException:
-            pass
-
-        wattsup.clearMemory()
-        wattsup.logInternal(1)
+        print('beginning trial {}-1-3 of {} (small.random, AES-GCM)'.format(trial, TRIALS))
+        print('waiting for write buffer flush...')
         
-        # Begin logging with Wattsup (above), run filebench (here), close out the
-        # Wattsup logger (below)
-        print("dd-write returned: ", subprocess.call(['/home/odroid/bd3/rsync/energy-AES-1/dd-write.sh', writeto, coreType, fsType], stdout=out))
-        print("dd-read returned: ", subprocess.call(['/home/odroid/bd3/rsync/energy-AES-1/dd-read.sh', writeto, coreType, fsType], stdout=out))
+        time.sleep(2)
 
-        # This loop handles any annoying errors we may encounter
-        while True:
-            try:
-                wattsup.printStats(wattsup.getInternalData(), out)
-                wattsup.serial.close()
-                break
+        print('beginning trial {}-1-4 of {} (small.random, ChaCha20-Poly1305)'.format(trial, TRIALS))
+        print('waiting for write buffer flush...')
+        
+        time.sleep(2)
 
-            except ValueError:
-                print('[+] recovered from ValueError')
-                wattsup.serial.close()
-                time.sleep(0.1) # Give Wattsup a moment to get its shit together
-                wattsup.serial.open()
-                continue
+        print('beginning trial {}-2-1 of {} (large.random, AES-CTR)'.format(trial, TRIALS))
+        print('waiting for write buffer flush...')
+        
+        time.sleep(2)
+
+        print('beginning trial {}-2-2 of {} (large.random, ChaCha20)'.format(trial, TRIALS))
+        print('waiting for write buffer flush...')
+
+        time.sleep(2)
+
+        print('beginning trial {}-2-3 of {} (large.random, AES-GCM)'.format(trial, TRIALS))
+        print('waiting for write buffer flush...')
+        
+        time.sleep(2)
+
+        print('beginning trial {}-2-4 of {} (large.random, ChaCha20-Poly1305)'.format(trial, TRIALS))
+        print('waiting for write buffer flush...')
+
+        time.sleep(2)
+
+        
         print('trial {}/{} complete'.format(trial, TRIALS))
 
 wattsup.serial.close()
