a0bcfb39f32d2d662545b1822adcd3e61b8cd7f7
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2016-03-22 05:50:58 -0500

    made rspush/pull less annoying

--- rspull.sh
+++ rspull.sh
@@ -1,1 +1,1 @@
-rsync -chavzP --stats odroid3:/home/odroid/bd3/rsync/energy-AES-1/ ../energy-AES-1
+rsync -chavzP --stats --exclude '.git' odroid3:/home/odroid/bd3/rsync/energy-AES-1/ ../energy-AES-1
--- rspush.sh
+++ rspush.sh
@@ -1,1 +1,1 @@
-rsync -chavzP --stats ../energy-AES-1/ odroid3:/home/odroid/bd3/rsync/energy-AES-1
+rsync -chavzP --stats --exclude '.git' ../energy-AES-1/ odroid3:/home/odroid/bd3/rsync/energy-AES-1
