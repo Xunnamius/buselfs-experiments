a3046108676fcf45816b6120f742966ce15cb8dc
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-05-12 12:48:48 -0500

    made the dd scripts less onerous

--- dd-read.sh
+++ dd-read.sh
@@ -5,4 +5,5 @@ coretype=$2
 fstype=$3
 
 echo 1 | sudo tee /proc/sys/vm/drop_caches
-`dd if=${writeto} of=/dev/null bs=1024 count=20000 iflag=direct >> results/shmoo.${coretype}.${fstype}.results 2>&1`
+#`dd if=${writeto} of=/dev/null bs=1024 count=20000 iflag=direct >> results/shmoo.${coretype}.${fstype}.results 2>&1`
+`dd if=${writeto} of=/dev/null bs=1024 count=4096 iflag=direct 2>&1`
