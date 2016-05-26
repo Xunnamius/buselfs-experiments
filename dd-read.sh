b2ae316094c3f733eb7596ce4750b4d76688d4cd
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-05-26 13:33:21 -0500

    monday updates

--- dd-read.sh
+++ dd-read.sh
@@ -6,4 +6,4 @@ fstype=$3
 
 echo 1 | sudo tee /proc/sys/vm/drop_caches
 #`dd if=${writeto} of=/dev/null bs=1024 count=20000 iflag=direct >> results/shmoo.${coretype}.${fstype}.results 2>&1`
-`dd if=${writeto} of=/dev/null bs=1024 count=4096 iflag=direct 2>&1`
+`dd if=${writeto} of=/dev/null bs=512 count=25000 iflag=direct 2>&1`
