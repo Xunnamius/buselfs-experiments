83de5ec29af07adf35e98340a6005a5dc1166ccd
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-05-28 06:47:56 -0500

    untested setupfilesystem.sh; fixed dd issue

--- dd-read.sh
+++ dd-read.sh
@@ -5,5 +5,4 @@ coretype=$2
 fstype=$3
 
 echo 1 | sudo tee /proc/sys/vm/drop_caches
-#`dd if=${writeto} of=/dev/null bs=1024 count=20000 iflag=direct >> results/shmoo.${coretype}.${fstype}.results 2>&1`
-`dd if=${writeto} of=/dev/null bs=512 count=25000 iflag=direct`
+`dd if=${writeto} of=/dev/null bs=512 count=25000 iflag=direct >> results/shmoo.${coretype}.${fstype}.results`
