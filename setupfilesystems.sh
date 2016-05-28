9e68eae1d9d2deca66cdc0dc34b172e63a265c49
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-05-28 07:59:55 -0500

    sudo

--- setupfilesystems.sh
+++ setupfilesystems.sh
@@ -9,71 +9,71 @@ fi
 
 # Creates ram0
 echo 'Creating (1) ram disk of size 512MB'
-modprobe brd rd_nr=1 rd_size=524288
+sudo modprobe brd rd_nr=1 rd_size=524288
 
 echo 'Making a filesystem atop the ram disk'
-mkfs.ext4 /dev/ram0
+sudo mkfs.ext4 /dev/ram0
 
 echo 'Mounting to /media/rd'
-mount -t ext4 /dev/ram0 /media/rd
+sudo mount -t ext4 /dev/ram0 /media/rd
 
 echo 'Setting up and mounting (trivial) directory structure 07 on ram disk'
-mkdir -p /media/rd/07-rdext4-normal/write
+sudo mkdir -p /media/rd/07-rdext4-normal/write
 
 echo 'Setting up and mounting directory structure 08 on ram disk'
-mkdir -p /media/rd/08-rdext4-fuse-ext4/write
-dd if=/dev/zero of=/media/rd/08-rdext4-fuse-ext4/fs.ext4 bs=64M count=1
-mkfs.ext4 /media/rd/08-rdext4-fuse-ext4/fs.ext4
-fuse-ext2 /media/rd/08-rdext4-fuse-ext4/fs.ext4 /media/rd/08-rdext4-fuse-ext4/write -o rw+
+sudo mkdir -p /media/rd/08-rdext4-fuse-ext4/write
+sudo dd if=/dev/zero of=/media/rd/08-rdext4-fuse-ext4/fs.ext4 bs=64M count=1
+sudo mkfs.ext4 /media/rd/08-rdext4-fuse-ext4/fs.ext4
+sudo fuse-ext2 /media/rd/08-rdext4-fuse-ext4/fs.ext4 /media/rd/08-rdext4-fuse-ext4/write -o rw+
 
 echo 'Setting up and mounting directory structure 09 on ram disk'
-mkdir -p /media/rd/09-rdext4-fuse-ext4-dmc/write
-dd if=/dev/zero of=/media/rd/09-rdext4-fuse-ext4-dmc/fs.ext4 bs=80M count=1
-mkfs.ext4 /media/rd/09-rdext4-fuse-ext4-dmc/fs.ext4
-fuse-ext2 /media/rd/09-rdext4-fuse-ext4-dmc/fs.ext4 /media/rd/09-rdext4-fuse-ext4-dmc/write -o rw+
-mkdir /media/rd/09-rdext4-fuse-ext4-dmc/write/write
-dd if=/dev/zero of=/media/rd/09-rdext4-fuse-ext4-dmc/write/fs.dmc bs=64M count=1
-# badpasswordsarebad is the password
-cryptsetup --verbose --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 5000 --use-random luksFormat /media/rd/09-rdext4-fuse-ext4-dmc/write/fs.dmc
-cryptsetup open --type luks /media/rd/09-rdext4-fuse-ext4-dmc/write/fs.dmc 09.fs.dmc
-mkfs.ext4 /dev/mapper/09.fs.dmc
-mount -t ext4 /dev/mapper/09.fs.dmc /media/rd/09-rdext4-fuse-ext4-dmc/write/write
+sudo mkdir -p /media/rd/09-rdext4-fuse-ext4-dmc/write
+sudo dd if=/dev/zero of=/media/rd/09-rdext4-fuse-ext4-dmc/fs.ext4 bs=80M count=1
+sudo mkfs.ext4 /media/rd/09-rdext4-fuse-ext4-dmc/fs.ext4
+sudo fuse-ext2 /media/rd/09-rdext4-fuse-ext4-dmc/fs.ext4 /media/rd/09-rdext4-fuse-ext4-dmc/write -o rw+
+sudo mkdir /media/rd/09-rdext4-fuse-ext4-dmc/write/write
+sudo dd if=/dev/zero of=/media/rd/09-rdext4-fuse-ext4-dmc/write/fs.dmc bs=64M count=1
+sudo # badpasswordsarebad is the password
+sudo cryptsetup --verbose --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 5000 --use-random luksFormat /media/rd/09-rdext4-fuse-ext4-dmc/write/fs.dmc
+sudo cryptsetup open --type luks /media/rd/09-rdext4-fuse-ext4-dmc/write/fs.dmc 09.fs.dmc
+sudo mkfs.ext4 /dev/mapper/09.fs.dmc
+sudo mount -t ext4 /dev/mapper/09.fs.dmc /media/rd/09-rdext4-fuse-ext4-dmc/write/write
 
 echo 'Setting up and mounting directory structure 10 on ram disk'
-mkdir -p /media/rd/10-rdext4-dmc-fuse-ext4/write
-dd if=/dev/zero of=/media/rd/10-rdext4-dmc-fuse-ext4/fs.dmc bs=80M count=1
-# badpasswordsarebad is the password
-cryptsetup --verbose --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 5000 --use-random luksFormat /media/rd/10-rdext4-dmc-fuse-ext4/fs.dmc
-cryptsetup open --type luks /media/rd/10-rdext4-dmc-fuse-ext4/fs.dmc 10.fs.dmc
-mkfs.ext4 /dev/mapper/10.fs.dmc
-mount -t ext4 /dev/mapper/10.fs.dmc /media/rd/10-rdext4-dmc-fuse-ext4/write
-mkdir /media/rd/10-rdext4-dmc-fuse-ext4/write/write
-dd if=/dev/zero of=/media/rd/10-rdext4-dmc-fuse-ext4/write/fs.ext4 bs=64M count=1
-mkfs.ext4 /media/rd/10-rdext4-dmc-fuse-ext4/write/fs.ext4
-fuse-ext2 /media/rd/10-rdext4-dmc-fuse-ext4/write/fs.ext4 /media/rd/10-rdext4-dmc-fuse-ext4/write/write -o rw+
+sudo mkdir -p /media/rd/10-rdext4-dmc-fuse-ext4/write
+sudo dd if=/dev/zero of=/media/rd/10-rdext4-dmc-fuse-ext4/fs.dmc bs=80M count=1
+sudo # badpasswordsarebad is the password
+sudo cryptsetup --verbose --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 5000 --use-random luksFormat /media/rd/10-rdext4-dmc-fuse-ext4/fs.dmc
+sudo cryptsetup open --type luks /media/rd/10-rdext4-dmc-fuse-ext4/fs.dmc 10.fs.dmc
+sudo mkfs.ext4 /dev/mapper/10.fs.dmc
+sudo mount -t ext4 /dev/mapper/10.fs.dmc /media/rd/10-rdext4-dmc-fuse-ext4/write
+sudo mkdir /media/rd/10-rdext4-dmc-fuse-ext4/write/write
+sudo dd if=/dev/zero of=/media/rd/10-rdext4-dmc-fuse-ext4/write/fs.ext4 bs=64M count=1
+sudo mkfs.ext4 /media/rd/10-rdext4-dmc-fuse-ext4/write/fs.ext4
+sudo fuse-ext2 /media/rd/10-rdext4-dmc-fuse-ext4/write/fs.ext4 /media/rd/10-rdext4-dmc-fuse-ext4/write/write -o rw+
 
 echo 'Setting up and mounting directory structure 11 on ram disk'
-mkdir -p /media/rd/11-rdext4-fuse-lfs/write
+sudo mkdir -p /media/rd/11-rdext4-fuse-lfs/write
 ## todo
 
 echo 'Setting up and mounting directory structure 12 on ram disk'
-mkdir -p /media/rd/12-rdext4-fuse-lfs-chacha-poly/write
+sudo mkdir -p /media/rd/12-rdext4-fuse-lfs-chacha-poly/write
 ## todo
 
 echo 'Mounting filesystem 01 (trivial)'
 
 echo 'Mounting filesystem 02'
-fuse-ext2 /home/odroid/bd3/fuse/mnt/02-kext4-fuse-ext4/fs.ext4 /home/odroid/bd3/fuse/mnt/02-kext4-fuse-ext4/write -o rw+
+sudo fuse-ext2 /home/odroid/bd3/fuse/mnt/02-kext4-fuse-ext4/fs.ext4 /home/odroid/bd3/fuse/mnt/02-kext4-fuse-ext4/write -o rw+
 
 echo 'Mounting filesystem 03'
-fuse-ext2 /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/fs.ext4 /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/write -o rw+
-cryptsetup open --type luks /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/write/fs.dmc 03.fs.dmc
-mount -t ext4 /dev/mapper/03.fs.dmc /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/write/write
+sudo fuse-ext2 /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/fs.ext4 /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/write -o rw+
+sudo cryptsetup open --type luks /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/write/fs.dmc 03.fs.dmc
+sudo mount -t ext4 /dev/mapper/03.fs.dmc /home/odroid/bd3/fuse/mnt/03-kext4-fuse-ext4-dmc/write/write
 
 echo 'Mounting filesystem 04'
-cryptsetup open --type luks /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/fs.dmc 04.fs.dmc
-mount -t ext4 /dev/mapper/04.fs.dmc /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/write
-fuse-ext2 /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/write/fs.ext4 /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/write/write -o rw+
+sudo cryptsetup open --type luks /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/fs.dmc 04.fs.dmc
+sudo mount -t ext4 /dev/mapper/04.fs.dmc /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/write
+sudo fuse-ext2 /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/write/fs.ext4 /home/odroid/bd3/fuse/mnt/04-kext4-dmc-fuse-ext/write/write -o rw+
 
 echo 'Mounting filesystem 05'
 ## todo
