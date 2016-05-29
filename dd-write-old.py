c0738d8084b4be4150edc28adcc8cb65fca34f49
Xunnamius (Dynasty) <Xunnamius@users.noreply.github.com>
2016-05-29 18:48:12 -0500

    my foolishness...

--- dd-write-old.py
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-writeto=$1
-coretype=$2
-fstype=$3
-
-`dd if=/dev/zero of=${writeto} bs=512 count=25000 conv=fdatasync oflag=direct >> results/shmoo.${coretype}.${fstype}.results 2>&1`
