e86c578d26e6df45707b94f6951f219b99411b4d
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2016-12-02 05:48:13 -0600

    reduced rounds back to 75mb per round

--- dd-read.sh
+++ dd-read.sh
@@ -1,2 +1,2 @@
 echo 3 | sudo tee /proc/sys/vm/drop_caches
-dd if=$1 of=/dev/null bs=1M count=100 #iflag=direct
+dd if=$1 of=/dev/null bs=1M count=75 #iflag=direct
--- dd-write.sh
+++ dd-write.sh
@@ -1,1 +1,1 @@
-dd if=/dev/zero of=$1 bs=1M count=100 conv=fdatasync
+dd if=/dev/zero of=$1 bs=1M count=75 conv=fdatasync
