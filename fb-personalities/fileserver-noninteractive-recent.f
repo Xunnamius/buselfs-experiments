9ff10a40d0d325df4039a68920b54a2ccdbbfa2b
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2017-04-12 14:43:27 -0500

    direct io version of personalities

--- fb-personalities/fileserver-noninteractive-recent.f
+++ fb-personalities/fileserver-noninteractive-recent.f
@@ -37,13 +37,13 @@ define process name=filereader,instances=1
 {
   thread name=filereaderthread,memsize=10m,instances=$nthreads
   {
-    flowop createfile name=createfile1,filesetname=bigfileset,fd=1
-    flowop writewholefile name=wrtfile1,srcfd=1,fd=1,iosize=$iosize
+    flowop createfile name=createfile1,filesetname=bigfileset,fd=1,directio,dsync
+    flowop writewholefile name=wrtfile1,srcfd=1,fd=1,iosize=$iosize,dsync
     flowop closefile name=closefile1,fd=1
-    flowop openfile name=openfile1,filesetname=bigfileset,fd=1
-    flowop appendfilerand name=appendfilerand1,iosize=$meanappendsize,fd=1
+    flowop openfile name=openfile1,filesetname=bigfileset,fd=1,directio,dsync
+    flowop appendfilerand name=appendfilerand1,iosize=$meanappendsize,fd=1,dsync
     flowop closefile name=closefile2,fd=1
-    flowop openfile name=openfile2,filesetname=bigfileset,fd=1
+    flowop openfile name=openfile2,filesetname=bigfileset,fd=1,directio,dsync
     flowop readwholefile name=readfile1,fd=1,iosize=$iosize
     flowop closefile name=closefile3,fd=1
     flowop deletefile name=deletefile1,filesetname=bigfileset
