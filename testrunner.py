a9f36656571c108ccfff716f74fdcdaed84f9483
Xunnamius (Morty) <Xunnamius@users.noreply.github.com>
2017-09-15 10:49:23 +0000

    gcon

--- testrunner.py
+++ testrunner.py
@@ -461,7 +461,8 @@ if __name__ == "__main__":
 
         configurations = (
             #Configuration('nilfs2', 'nilfs2', []),
-            Configuration('f2fs', 'f2fs', ['-o', 'background_gc_off']),
+            #Configuration('f2fs', 'f2fs', ['-o', 'background_gc_off']),
+            Configuration('f2fs', 'f2fs', []),
             #Configuration('ext4-oj', 'ext4', []),
             #Configuration('ext4-fj', 'ext4', ['-o', 'data=journal'])
         )
