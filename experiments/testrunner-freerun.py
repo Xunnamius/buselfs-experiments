1a2d222e0e6a1118b717b9c158098104d04f8898
Xunnamius (Morty) <Xunnamius@users.noreply.github.com>
2018-06-26 18:58:05 +0000

    changed filesystems in freerun

--- experiments/testrunner-freerun.py
+++ experiments/testrunner-freerun.py
@@ -14,7 +14,7 @@ lib = Librunner(config)
 num_nbd_devices = 16
 num_nbd_device = 0
 
-filesystems = ['nilfs2', 'f2fs']
+filesystems = ['f2fs'] #[f2fs']
 filesizes = ['1k', '512k', '40m'] #['1k', '4k', '512k', '5m', '40m']
 
 experiments = [lib.sequentialFreerun] #[lib.sequentialFreerun, lib.randomFreerun]
