beb4cf45907a558ee6fc92597bd27eaa7f289d26
Xunnamius (Morty) <Xunnamius@users.noreply.github.com>
2018-07-17 14:01:58 -0500

    updated for neon tests

--- experiments/testrunner-optimal-flknug.py
+++ experiments/testrunner-optimal-flknug.py
@@ -19,19 +19,23 @@ filesystems = ['f2fs', 'nilfs2']
 dataClasses = ['1k', '4k', '512k', '5m', '40m']
 
 flksizes = [512, 1024, 2048, 4096, 8192, 16384]
-fpns = [8, 16, 32, 64, 128] #256
+fpns = [8, 16, 32, 64, 128, 256]
 
 # TODO: add stringified names to experiments (tuples?)
 experiments = [lib.sequentialFreerun, lib.randomFreerun]
 
-ciphers = ['sc_salsa8',
-           'sc_salsa12',
-           'sc_salsa20',
-           'sc_aes128_ctr',
-           'sc_aes256_ctr',
-           #'sc_hc128', # ! too slow to test (see buselfs source for rationale)
-           'sc_rabbit',
-           'sc_sosemanuk'
+# ciphers = ['sc_salsa8',
+#            'sc_salsa12',
+#            'sc_salsa20',
+#            'sc_aes128_ctr',
+#            'sc_aes256_ctr',
+#            #'sc_hc128', # ! too slow to test (see buselfs source for rationale)
+#            'sc_rabbit',
+#            'sc_sosemanuk'
+# ]
+ciphers = ['sc_chacha20_neon',
+           'sc_chacha12_neon',
+           'sc_chacha8_neon'
 ]
 
 backendFnTuples = [
