a99e6f64414bd9b48c3cbe98653f10be3f79819f
debian-odroid3 <Xunnamius@users.noreply.github.com>
2019-05-08 23:51:48 -0500

    updated

--- experiments/testrunner-optimal-flknug.py
+++ experiments/testrunner-optimal-flknug.py
@@ -70,7 +70,7 @@ ciphers = [#'sc_salsa8',
            'sc_chacha8_neon',
            'sc_freestyle_fast',
            #'sc_freestyle_balanced',
-           'sc_freestyle_secure',
+           #'sc_freestyle_secure',
 ]
 
 backendFnTuples = [
