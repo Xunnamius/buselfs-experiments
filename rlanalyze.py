f470c21bd28a70debdf0267c5f55803f581a8d07
Xunnamius (Morty) <Xunnamius@users.noreply.github.com>
2019-09-02 05:22:51 -0500

    laptop syncback

--- rlanalyze.py
+++ rlanalyze.py
@@ -2,10 +2,35 @@
 
 RUNNER_LOG_PATH = '/tmp/runner.log'
 
+withinInterestingRegion = False
+interestingRegions = []
+
 if __name__ == "__main__":
     with open(RUNNER_LOG_PATH, 'r') as lines:
         for currentLine in lines:
-            if currentLine.find('got start time (ns):'):
+            if 'got start time (ns):' in currentLine:
                 withinInterestingRegion = True
-            if currentLine.find('got end time (ns):'):
-                withinInterestingRegion = True
+                interestingRegionName = currentLine[0:currentLine.index('METRICS') - 1]
+                interestingRegions.append({ 'name': interestingRegionName, 'requestBytes': 0, 'otherBytes': 0, 'swapped': False })
+            elif 'got end time (ns):' in currentLine:
+                withinInterestingRegion = False
+            elif withinInterestingRegion and 'Request for' in currentLine:
+                interestingRegion = interestingRegions[-1]
+                if ('write' in currentLine and 'WRITE' in interestingRegion['name'] or
+                    'read' in currentLine and 'READ' in interestingRegion['name']):
+                    interestingRegion['requestBytes'] += int(currentLine.split()[-1])
+                else:
+                    interestingRegion['otherBytes'] += int(currentLine.split()[-1])
+            elif 'Write was successful' in currentLine:
+                interestingRegions[-1]['swapped'] = True
+
+    for region in interestingRegions:
+        print('Region: {} has {} MB ({} bytes, {} ignored bytes)'.format(
+            region['name'],
+            round(region['requestBytes']/1024/1024, 1),
+            region['requestBytes'],
+            region['otherBytes']
+        ))
+
+        if region['swapped']:
+            print('(swapped!)')
