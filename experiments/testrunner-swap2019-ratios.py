0f500bb49ed390c7cb2065cd5e056b4c78f0b91a
debian-odroid3 <Xunnamius@users.noreply.github.com>
2019-09-17 20:56:01 -0500

    results syncback

--- experiments/testrunner-swap2019-ratios.py
+++ experiments/testrunner-swap2019-ratios.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
         for filesystem in filesystems:
             for fpn in fpns:
                 for flk_size in flksizes:
-                    for swap_ratio in range(1, 2): # ! (1, 4) means 1 through 3 inclusive!
+                    for swap_ratio in range(1, 4): # ! (1, 4) means 1 through 3 inclusive!
                         configurations.extend([
                             ExtendedConfiguration(
                                 '{}#{}#{}#{}#{}#{}+{}'.format(filesystem, cipherpair[0], flk_size, fpn, cipherpair[1], cipherpair[2], swap_ratio),
