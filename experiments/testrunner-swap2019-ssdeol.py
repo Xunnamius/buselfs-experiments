4d505b3eca7b53c4aaf22186b5a456a4181203d4
Xunnamius (Zara) <Xunnamius@users.noreply.github.com>
2019-10-12 02:55:00 -0500

    bug smashin

--- experiments/testrunner-swap2019-ssdeol.py
+++ experiments/testrunner-swap2019-ssdeol.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# pylint: disable=no-member
 
 import os
 import sys
@@ -16,7 +15,6 @@ lib = Librunner(config)
 
 ### * Configurables * ###
 
-KEEP_RUNNER_LOGS = False
 DIE_ON_EXCEPTION = True
 
 # ! REMEMBER: it's nilfs2 (TWO) with a 2! Not just 'nilfs'!
@@ -57,46 +55,31 @@ fpns = [
     #256,
 ]
 
+# TODO: add stringified names to experiments (tuples?)
 experiments = [
     lib.sequentialFreerun,
-    #lib.randomFreerun,
 ]
 
-# ? These are all the cipher swapping pairs that will be tested
-# ? each element: (primary cipher, swap cipher, swap strategy)
-cipherpairs = [
-    #('sc_freestyle_balanced', 'sc_chacha8_neon', 'swap_0_forward'),
-
-    ('sc_chacha8_neon', 'sc_chacha20_neon', 'swap_0_forward'),
-    #('sc_chacha8_neon', 'sc_freestyle_fast', 'swap_0_forward'),
-    ('sc_chacha20_neon', 'sc_freestyle_fast', 'swap_0_forward'),
-    ('sc_freestyle_fast', 'sc_freestyle_balanced', 'swap_0_forward'),
-    ('sc_freestyle_balanced', 'sc_freestyle_secure', 'swap_0_forward'),
-
-    # ('sc_chacha8_neon', 'sc_chacha20_neon', 'swap_1_forward'),
-    # #('sc_chacha8_neon', 'sc_freestyle_fast', 'swap_1_forward'),
-    # ('sc_chacha20_neon', 'sc_freestyle_fast', 'swap_1_forward'),
-    # ('sc_freestyle_fast', 'sc_freestyle_balanced', 'swap_1_forward'),
-    # ('sc_freestyle_balanced', 'sc_freestyle_secure', 'swap_1_forward'),
-
-    # ('sc_chacha8_neon', 'sc_chacha20_neon', 'swap_2_forward'),
-    # #('sc_chacha8_neon', 'sc_freestyle_fast', 'swap_2_forward'),
-    # ('sc_chacha20_neon', 'sc_freestyle_fast', 'swap_2_forward'),
-    # ('sc_freestyle_fast', 'sc_freestyle_balanced', 'swap_2_forward'),
-    # ('sc_freestyle_balanced', 'sc_freestyle_secure', 'swap_2_forward'),
-
-    # ('sc_chacha8_neon', 'sc_chacha20_neon', 'swap_mirrored'),
-    # #('sc_chacha8_neon', 'sc_freestyle_fast', 'swap_mirrored'),
-    # ('sc_chacha20_neon', 'sc_freestyle_fast', 'swap_mirrored'),
-    # ('sc_freestyle_fast', 'sc_freestyle_balanced', 'swap_mirrored'),
-    # ('sc_freestyle_balanced', 'sc_freestyle_secure', 'swap_mirrored')
+ciphers = [#'sc_salsa8',
+           #'sc_salsa12',
+           #'sc_aes128_ctr',
+           #'sc_aes256_ctr',
+           #'sc_hc128', # ! too slow to test (see buselfs source for rationale)
+           #'sc_rabbit',
+           #'sc_sosemanuk',
+           'sc_chacha20_neon',
+           #'sc_chacha12_neon',
+           'sc_chacha8_neon',
+           'sc_freestyle_fast',
+           'sc_freestyle_balanced',
+           'sc_freestyle_secure',
 ]
 
 backendFnTuples = [
     #(lib.createRawBackend, lib.destroyRawBackend, 'raw-vanilla'),
     #(lib.createVanillaBackend, lib.destroyVanillaBackend, 'vanilla'),
     #(lib.createDmcBackend, lib.destroyDmcBackend, 'dmcrypt'),
-    (lib.createSbBackend, lib.destroySbBackend, 'strongbox')
+    (lib.createSbBackend, lib.destroySbBackend, 'strongbox'),
 ]
 
 ### *** ###
@@ -120,40 +103,36 @@ if __name__ == "__main__":
         lib.clearBackstoreFiles()
         lib.print('constructing configurations')
 
-        # * And it begins!
+        # * Optimal flake/nugget size perf test
         configurations = []
         for filesystem in filesystems:
             for fpn in fpns:
                 for flk_size in flksizes:
                     configurations.extend([
                         Configuration(
-                            '{}#{}#{}#{}#{}#{}'.format(filesystem, cipherpair[0], flk_size, fpn, cipherpair[1], cipherpair[2]),
+                            '{}#{}#{}#{}'.format(filesystem, cipher, flk_size, fpn),
                             filesystem,
                             [],
                             [
-                                '--cipher', cipherpair[0],
+                                '--cipher', cipher,
                                 '--flake-size', str(flk_size),
-                                '--flakes-per-nugget', str(fpn),
-                                '--swap-cipher', cipherpair[1],
-                                '--swap-strategy', cipherpair[2]
+                                '--flakes-per-nugget', str(fpn)
                             ]
-                        ) for cipherpair in cipherpairs
+                        ) for cipher in ciphers
                     ])
 
                     configurations.extend([
                         Configuration(
-                            '{}#{}#{}#{}#{}#{}'.format('delayed_{}'.format(filesystem), cipherpair[0], flk_size, fpn, cipherpair[1], cipherpair[2]),
+                            '{}#{}#{}#{}'.format('delayed_{}'.format(filesystem), cipher, flk_size, fpn),
                             filesystem,
                             [],
                             [
-                                '--cipher', cipherpair[0],
+                                '--cipher', cipher,
                                 '--flake-size', str(flk_size),
                                 '--flakes-per-nugget', str(fpn),
-                                '--swap-cipher', cipherpair[1],
-                                '--swap-strategy', cipherpair[2],
                                 '--delay-rw'
                             ]
-                        ) for cipherpair in cipherpairs
+                        ) for cipher in ciphers
                     ])
 
         confcount = len(configurations) * len(backendFnTuples) * len(dataClasses) * len(experiments)
@@ -168,8 +147,6 @@ if __name__ == "__main__":
                     for backendFn in backendFnTuples:
                         for runFn in experiments:
                             for dataClass in dataClasses:
-                                identifier = '[unknown]'
-
                                 with open(config['LOG_FILE_PATH'], 'w') as file:
                                     print(str(datetime.now()), '\n---------\n', file=file)
 
@@ -237,18 +214,6 @@ if __name__ == "__main__":
                                     lib.logFile = None
 
                                     progressBar.update()
-
-                                if KEEP_RUNNER_LOGS:
-                                        filename, fileext = os.path.splitext(os.path.basename(config['LOG_FILE_PATH']))
-                                        os.rename(
-                                            config['LOG_FILE_PATH'],
-                                            '{}/{}-{}{}'.format(
-                                                os.path.dirname(config['LOG_FILE_PATH']),
-                                                filename,
-                                                identifier,
-                                                fileext
-                                            )
-                                        )
 
         lib.print('done', severity='OK')
 
