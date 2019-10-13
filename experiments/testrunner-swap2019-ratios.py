b95479761730cfc9e6856239c6be5c8edb8c04aa
Xunnamius (Zara) <Xunnamius@users.noreply.github.com>
2019-10-13 16:57:18 -0500

    gather some more

--- experiments/testrunner-swap2019-ratios.py
+++ experiments/testrunner-swap2019-ratios.py
@@ -64,7 +64,8 @@ experiments = [
 # ? These are all the cipher swapping pairs that will be tested
 # ? each element: (primary cipher, swap cipher, swap strategy)
 cipherpairs = [
-    ('sc_chacha8_neon', 'sc_freestyle_secure', 'swap_0_forward'),
+    ('sc_chacha8_neon', 'sc_chacha12_neon', 'swap_0_forward'),
+    ('sc_freestyle_balanced', 'sc_freestyle_secure', 'swap_0_forward'),
 
     # ('sc_chacha8_neon', 'sc_chacha20_neon', 'swap_0_forward'),
     # #('sc_chacha8_neon', 'sc_freestyle_fast', 'swap_0_forward'),
