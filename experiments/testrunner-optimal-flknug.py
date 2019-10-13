ecb5f3598a4fb0c0c063bbdee243fb628f66d587
Xunnamius (Zara) <Xunnamius@users.noreply.github.com>
2019-10-13 16:54:38 -0500

    gather some more

--- experiments/testrunner-optimal-flknug.py
+++ experiments/testrunner-optimal-flknug.py
@@ -72,11 +72,11 @@ ciphers = [
     #'sc_rabbit',
     #'sc_sosemanuk',
     #'sc_chacha20_neon',
-    #'sc_chacha12_neon',
-    'sc_chacha8_neon',
+    'sc_chacha12_neon',
+    #'sc_chacha8_neon',
     #'sc_freestyle_fast',
-    #'sc_freestyle_balanced',
-    'sc_freestyle_secure',
+    'sc_freestyle_balanced',
+    #'sc_freestyle_secure',
 ]
 
 backendFnTuples = [
